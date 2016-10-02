5f5cd452730cfaa5672d0020904db8c3cc71bee9
amazingandyyy <amazingandyyy@gmail.com>
2016-10-01 17:25:46 -0700

    make it better

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var clock_info = {};
 /* GET users listing. */
 router.post('/setclock', function(req, res) {
-  console.log(req.body);
-  res.send(req.body)
+  clock_info = req.body
+  console.log('clock_info: ', clock_info);
+  res.send(clock_info)
+});
+router.get('/getlatestclock', function(req, res) {
+  res.send(clock_info)
 });
 
 module.exports = router;
